639051d187784348661c6e97f36a0d3fa086eda1
Demo <76752016+DemoExists@users.noreply.github.com>
2022-09-19 19:37:50 +0200

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,7 @@
+--[[
+    Notification Library made by demo#0293
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DemoExists/Drawing-Notifications/main/main.lua", true))()
+
+Notification.New("Example Notification", 5, Color3.fromRGB(255, 0, 0))
